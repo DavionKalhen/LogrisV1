53500dd6b0dc5612666cb3a0171851c3ca135013
LogrisTheBard <logristhebard@gmail.com>
2023-02-26 20:05:10 -0700

    added alETH to ETH swap

--- test/CurveAlETHSwapTest.t.sol
+++ test/CurveAlETHSwapTest.t.sol
@@ -12,6 +12,7 @@ contract CurveAlETHSwapTest is Test {
     address alETHAddress = 0x0100546F2cD4C9D97f798fFC9755E47865FF7Ee6;
     IERC20 alETHToken;
     IStableMetaPool curvePool;
+    //i,j determined by coins view method on contract
     int128 ethPoolIndex = 0;
     int128 alETHPoolIndex = 1;
 
@@ -21,13 +22,29 @@ contract CurveAlETHSwapTest is Test {
         alETHToken = IERC20(alETHAddress);
     }
 
-    //i,j determined by coins view method on contract
     function testETHtoAlETHSwap() public {
-        uint256 amountToTrade = 100;
-        uint256 minReceived = 50;
+        uint256 amountToTrade = 10000000000000000000;
+        uint256 minReceived =    5000000000000000000;
 
-        uint256 output = curvePool.exchange(ethPoolIndex, alETHPoolIndex, amountToTrade, minReceived);
+        uint256 output = curvePool.exchange{value:10 ether}(ethPoolIndex, alETHPoolIndex, amountToTrade, minReceived);
         require(output>minReceived);
         require(alETHToken.balanceOf(address(this))>0, "no coins in address");
+    }
+
+    function testAlETHtoETHSwap() public {
+        testETHtoAlETHSwap();
+        alETHToken.approve(alETHCurvePoolAddress, type(uint256).max);
+        uint256 alETHBalance = alETHToken.balanceOf(address(this));
+        require(alETHBalance>0, "no alETH to spend");
+        uint256 amountToTrade = alETHBalance;
+        uint256 minReceived = amountToTrade/2;
+        uint256 output = curvePool.exchange{value:0 ether}(alETHPoolIndex, ethPoolIndex, amountToTrade, minReceived);
+        require(output>minReceived);
+        require(alETHToken.balanceOf(address(this))==0, "failed to spend all coins");
+    }
+
+    //callback when the output of the swap is eth.
+    fallback() payable external {
+        console.log("Received");
     }
 }
