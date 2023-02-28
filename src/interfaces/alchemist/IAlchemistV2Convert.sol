053e9412e6b584fad2eb56f1e8848dc0ac79efcc
LogrisTheBard <logristhebard@gmail.com>
2023-02-27 18:15:00 -0700

    extended alchemix interface

---
+++ src/interfaces/alchemist/IAlchemistV2Convert.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+interface IAlchemistV2Convert {
+    function convertYieldTokensToShares(address yieldToken, uint256 amount) external view returns (uint256);
+    function convertSharesToYieldTokens(address yieldToken, uint256 shares) external view returns (uint256);
+    function convertSharesToUnderlyingTokens(address yieldToken, uint256 shares) external view returns (uint256);
+    function convertYieldTokensToUnderlying(address yieldToken, uint256 amount) external view returns (uint256);
+    function convertUnderlyingTokensToYield(address yieldToken, uint256 amount) external view returns (uint256);
+    function convertUnderlyingTokensToShares(address yieldToken, uint256 amount) external view returns (uint256);
+}
